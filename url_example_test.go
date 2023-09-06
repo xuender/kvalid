fea6c4b356e81d1814d294a2eab48f2c4a43d540
xuender <xuender@gmail.com>
2023-09-06 11:24:01 +0800

    fix: url miss port

--- url_example_test.go
+++ url_example_test.go
@@ -11,9 +11,11 @@ func ExampleIsURL() {
 	fmt.Println(kvalid.IsURL("http//:example.com"))
 	fmt.Println(kvalid.IsURL("http://www.example.com"))
 	fmt.Println(kvalid.IsURL("ftp://aa:pp@www.example.com"))
+	fmt.Println(kvalid.IsURL("http://www.example.com:8080"))
 
 	// Output:
 	// false
+	// true
 	// true
 	// true
 }
